d75c16985bc5a9650179c64146a1d49f04bc10c8
newolad <newolad.development@gmail.com>
2025-04-12 14:17:46 -0500

    Delete all

--- TPCorte2/ejercicio1/script.js
+++
@@ -1,7 +0,0 @@
-let contador = 0;
-
-document.getElementById("btnIncrementar");
-btnIncrementar.addEventListener("click", () => {
-  contador++;
-  document.getElementById("contador").textContent = contador;
-});
--- TPCorte2/ejercicio3/script.js
+++
@@ -1,4 +0,0 @@
-const imagen = document.getElementById("imagen");
-document.getElementById("btnCambiar").addEventListener("click", () => {
-  imagen.src = imagen.src.includes("imagen1.jpg") ? "imagen2.jpg" : "imagen1.jpg";
-});
